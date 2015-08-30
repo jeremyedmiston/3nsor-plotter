ee07bd84138817a140b4711c64e8690df3bfc45f
Anton Vanhoucke <anton@fabrique.nl>
2015-08-30 22:19:30 +0200

    Added circular plot

--- brickpi_helpers.py
+++ brickpi_helpers.py
@@ -17,20 +17,27 @@ class plot_installation(object):
         self.h_margin = (l_rope_0 ** 2 - self.v_margin ** 2) ** 0.5  #pythagoras to find distance from triangle point to left doorframe
         self.canvas_size = attachment_distance - 2 * self.h_margin
 
-    def motor_targets_from_coords(self,x_norm, y_norm):
+    def motor_targets_from_norm_coords(self,x_norm, y_norm):
         x,y = self.normalized_to_global_coords(x_norm,y_norm)
+        return self.motor_targets_from_coords(x,y)
+
+    def motor_targets_from_coords(self,x, y):
         l_rope = (x ** 2 + y ** 2) ** 0.5
         r_rope = ((self.att_dist - x) ** 2 + y ** 2) ** 0.5
         l_target = (l_rope - self.l_rope_0) * self.cm_to_deg
         r_target = (r_rope - self.r_rope_0) * self.cm_to_deg
 
-        return l_target, r_target
+        return int(l_target), int(r_target)
 
     def coords_from_motor_pos(self,l_motor,r_motor):
         l_rope = l_motor / self.cm_to_deg + self.l_rope_0
         r_rope = r_motor / self.cm_to_deg + self.r_rope_0
-        x = (l_rope**2-nogiets**2)
+        y = self.triangle_area(l_rope,r_rope,self.att_dist)*2/self.att_dist
+        x = (l_rope**2 - y**2)**0.5
+        y_norm = (y - self.v_margin)/self.canvas_size
+        x_norm = (x - self.h_margin)/self.canvas_size
 
+        return x_norm,y_norm
 
     def normalized_to_global_coords(self,x_norm,y_norm):
         # convert normalized coordinates to global coordinates
@@ -39,7 +46,6 @@ class plot_installation(object):
 
         return x,y
 
-    # Calculate proportions of the rope triangle
     def triangle_area(self,a, b, c):
         """
         Calculate the area of a triangle by the lengths of it's sides using Heron's formula
@@ -113,7 +119,7 @@ class motorPID_control(object):
     motor power for a servo.
     """
 
-    def __init__(self, motor_port, KP=.6, KI=0.05, KD=0.0):
+    def __init__(self, motor_port, KP=1, KI=0.0, KD=0.0):
         self.port = motor_port
         self.Kp = KP
         self.Ki = KI
@@ -123,17 +129,32 @@ class motorPID_control(object):
         self.timestamp = time.time()
         self.zero = 0
         self.target = 0
-        self.position = 0
+        self.encoder = 0
 
     @property
     def error(self):
-        return self.target - (self.position - self.zero)
+        return self.target - self.position
+
+    @property
+    def position(self):
+        return self.encoder - self.zero
+
+    @property
+    def target(self):
+        return self.__target
+
+    @target.setter
+    def target(self, target):
+        self.__target = target
+        self.integral = 0
+        self.prev_error = 0
 
     def get_power(self):
         error = self.error
         dt = time.time() - self.timestamp
-        self.integral += error * dt
+        self.integral = error * dt
         derivative = (error - self.prev_error) / dt
+        #print self.port, error, self.integral, derivative
         output = self.Kp * error + self.Ki * self.integral + self.Kd * derivative
         self.prev_error = error
         self.timestamp = time.time()
--- plotter.py
+++ plotter.py
@@ -16,7 +16,7 @@ __author__ = 'anton'
 # ##############################################################################################################
 
 # CONNECTIONS-
-#   Pen Motor - Port A
+# Pen Motor - Port A
 # 	Left Motor  - Port B
 # 	Right Motor - Port C
 #
@@ -64,16 +64,16 @@ ROPE_ATTACHMENT_WIDTH = 90  # space between the two attachment points of the plo
 PULLEY_DIAMETER = 4.4
 
 
-################## Globals #################
+################## Globals. I know. #################
 
-c = 0  # movement command. Global. :S
-websockets = [] # list of open sockets. Global. :S
-
+c = 0  # movement command.
+websockets = []  # list of open sockets.
+drive_motors = []
 
 ################# Movement functions ######################
 
 def pen_up():
-    BrickPi.MotorSpeed[PORT_A] = 30
+    BrickPi.MotorSpeed[PORT_A] = 10
     BrickPiUpdateValues()
     time.sleep(0.3)
     BrickPi.MotorSpeed[PORT_A] = 0
@@ -81,7 +81,7 @@ def pen_up():
 
 
 def pen_down():
-    BrickPi.MotorSpeed[PORT_A] = -30
+    BrickPi.MotorSpeed[PORT_A] = -10
     BrickPiUpdateValues()
     time.sleep(0.3)
     BrickPi.MotorSpeed[PORT_A] = 0
@@ -95,68 +95,138 @@ def set_motor_zero():
         print "Encoder zero position set to", motor.zero, "For motor at port:", motor.port
 
 
+def move_to_coord(x,y,plotter):
+    motor_b_target, motor_c_target  = plotter.motor_targets_from_coords(x, y)
+    print "Moving to ", x, ",", y, "(At", motor_b_target, motor_c_target, ")"
+    move_to_targets((motor_b_target, motor_c_target))
+
+
 def move_to_norm_coord(x_norm, y_norm, plotter):
+    motor_b_target, motor_c_target = plotter.motor_targets_from_norm_coords(x_norm, y_norm)
+    print "Moving to ", x_norm, ",", y_norm, "(At", motor_b_target, motor_c_target, ")"
+    move_to_targets((motor_b_target, motor_c_target))
+
+
+def move_to_targets(targets):
+    global drive_motors
+
     # set targets
-    motor_B.target, motor_C.target = plotter.motor_targets_from_coords(x_norm, y_norm)
+    for i in range(2):
+        drive_motors[i].target = targets[i]
 
     # run motors until targets are reached
     while 1:
+        BrickPiUpdateValues()  # Ask BrickPi to update values for sensors/motors
         for motor in drive_motors:
             #get motor positions
-            motor.position = BrickPi.Encoder[motor.port]
+            motor.encoder = BrickPi.Encoder[motor.port]
+
             #set motor speed accordingly
-            BrickPi.MotorSpeed[motor.port] = motor.get_power()
+            speed = motor.get_power()
+            BrickPi.MotorSpeed[motor.port] = speed
+
+        if (abs(motor_C.error) < 3) and (abs(motor_B.error) < 3):
+            #we got where we want to be. Time for the next coordinate.
+            for motor in drive_motors:
+                BrickPi.MotorSpeed[motor.port] = 0
+            BrickPiUpdateValues()
+            break
 
         #We're done calculating and setting all motor speeds!
-        BrickPiUpdateValues()  # Ask BrickPi to update values for sensors/motors
         time.sleep(0.02)
 
-        if (motor_C.error < 2) and (motor_B.error < 2):
-            #we got where we want to be. Time for the next coordinate.
-            break
+
 
 
 def plot_from_file(filename, plotter):
     coords = open(filename)
     num_coords = int(coords.readline())  #coords contains it's length on the first line.
+    print num_coords
 
     #drive to the first coordinate
     pen_up()
+    # read from file
+    x_norm, y_norm = [float(n) for n in coords.readline().split(",")]
+    #move
+    move_to_norm_coord(x_norm, y_norm, plotter)
     pen_down()
-
     for i in range(num_coords - 1):
         # read from file
         x_norm, y_norm = [float(n) for n in coords.readline().split(",")]
-
         #move
         move_to_norm_coord(x_norm, y_norm, plotter)
 
     coords.close()
 
 
-def plot_circles(plotter, step=0.1):
-    #draw circles form the left side until we reach the bottom
-    for i in range(0, 1, step * 2):
-        move_to_norm_coord(0, step, plotter)
+def plot_circles(plotter, num_circles=12):
+    r_min = (plotter.h_margin**2+plotter.v_margin**2)**0.5
+    r_max = ((plotter.h_margin+plotter.canvas_size)**2 + (plotter.v_margin+plotter.canvas_size)**2)**0.5
+    r_step = (r_max-r_min)/num_circles
 
-        #turn on right motor, slowly
-        BrickPi.MotorSpeed[PORT_C] = 100
+    for i in range(1, num_circles, 2):
+        x = plotter.h_margin
+        y = ((r_min+r_step*i)**2 - plotter.h_margin ** 2) ** 0.5
+        print "y:",y, "vm:",plotter.v_margin,"rmin:",r_min,"step:",r_step
+        if y > plotter.v_margin+plotter.canvas_size:
+            x = ((r_min + r_step*i) ** 2 - (plotter.v_margin + plotter.canvas_size) ** 2) ** 0.5
+            y = plotter.v_margin+plotter.canvas_size
+            print "Phase 3: Rollin' up!"
+        else:
+            print "Phase 1: Rollin' up!"
+
+        move_to_coord(x,y,plotter)
+
+
+        #turn on right motor, slowly, to draw circles left to right
+        BrickPi.MotorSpeed[PORT_C] = -80
+        #Motor B is off, but let's get it's encoder first
+        motor_B.encoder = BrickPi.Encoder[motor_B.port]
 
         #calculate coordinates continuously until we reach the top, or right side of the canvas
+        while 1:
+            BrickPiUpdateValues()
+            motor_C.encoder = BrickPi.Encoder[motor_C.port]
+            x_norm, y_norm = plotter.coords_from_motor_pos(motor_B.position, motor_C.position)
+            print "Currently at", x_norm,y_norm
+            if y_norm <= 0: break # reached the top
+            if x_norm >= 1: break # reached the right side
+            time.sleep(0.02)
 
-        #move to the next circle
+        #Good, now move to the next point and roll down.
+        x = ((r_min + r_step*(i+1)) ** 2 - plotter.v_margin ** 2) ** 0.5
+        y = plotter.v_margin
 
-        #turn on right motor, slowly backwards
+        if x > plotter.h_margin + plotter.canvas_size:
+            x = plotter.h_margin + plotter.canvas_size
+            y = ((r_min+r_step*(i+1)) ** 2 - (plotter.h_margin+plotter.canvas_size) ** 2) ** 0.5
+            print "Phase 2: Moving down"
+        else:
+            print "Phase 1: Moving down"
 
-        #calculate coordinates continuously until we reach the left, or bottom side of the canvas
+        move_to_coord(x,y,plotter)
+        #turn on right motor, slowly to draw circles from right to left.
+        BrickPi.MotorSpeed[PORT_C] = 80
+        #Motor B is off, but let's get it's encoder first
+        motor_B.encoder = BrickPi.Encoder[motor_B.port]
 
-        #now draw circles from the bottom
+        #calculate coordinates continuously until we reach the top, or right side of the canvas
+        while 1:
+            BrickPiUpdateValues()
+            motor_C.encoder = BrickPi.Encoder[motor_C.port]
+            x_norm, y_norm = plotter.coords_from_motor_pos(motor_B.position, motor_C.position)
+            print "Currently at", x_norm,y_norm
+            if y_norm >= 1: break # reached the bottom
+            if x_norm <= 0: break # reached the left side
+            time.sleep(0.02)
+
+    print "Done drawing"
 
 
 ################# Set up web server & threads #####################
 
 
-# Initialize TOrnado to use 'GET' and load index.html
+# Initialize Tornado to use 'GET' and load index.html
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         loader = tornado.template.Loader(".")
@@ -189,7 +259,8 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
 def wsSend(message):
     for ws in websockets:
-        ws.write_message(message)
+        pass
+        # ws.write_message(message)
 
 
 application = tornado.web.Application([
@@ -204,51 +275,46 @@ class MotorThread(threading.Thread):
         self.motor_log = Logger("Motors")
         threading.Thread.__init__(self)
         self.plotter = plotter
+        self.throttle = Throttler(MOTOR_CMD_RATE)
 
     def run(self):
         global c
         print "Starting motor thread"
         while running:
-            try:
-                if c == 'lmf':
-                    print "Running left motor fwd"
-                    BrickPi.MotorSpeed[PORT_B] = 100  #Set the speed of MotorA (-255 to 255)
 
-                elif c == 'lmb':
-                    print "Running left motor back"
-                    BrickPi.MotorSpeed[PORT_B] = -100
-                elif c == 'rmf':
-                    print "Running right motor forward"
-                    BrickPi.MotorSpeed[PORT_C] = 100
-                elif c == 'rmb':
-                    print "Running right motor back"
-                    BrickPi.MotorSpeed[PORT_C] = -100
-                elif c == 'stop':
-                    print "Stopped"
-                    wsSend("Stopped")
-                    BrickPi.MotorSpeed[PORT_B] = 0
-                    BrickPi.MotorSpeed[PORT_C] = 0
-                elif c == 'plot':
-                    plot_from_file('coords.csv', self.plotter)
-                elif c == 'zero':
-                    set_motor_zero()
+            if c == 'lmf':
+                print "Running left motor fwd"
+                BrickPi.MotorSpeed[PORT_B] = 100  #Set the speed of MotorA (-255 to 255)
 
-                BrickPiUpdateValues()  # BrickPi updates the values for the motors
-                time.sleep(0.05)
-                #print "Values Updated"
-
-
-            except KeyboardInterrupt:  #Triggered by pressing Ctrl+C. Time to clean up.
-
-                #Shutting down all motors.
-                BrickPi.MotorSpeed[PORT_A] = 0
+            elif c == 'lmb':
+                print "Running left motor back"
+                BrickPi.MotorSpeed[PORT_B] = -100
+            elif c == 'rmf':
+                print "Running right motor forward"
+                BrickPi.MotorSpeed[PORT_C] = 100
+            elif c == 'rmb':
+                print "Running right motor back"
+                BrickPi.MotorSpeed[PORT_C] = -100
+            elif c == 'stop':
+                print "Stopped"
                 BrickPi.MotorSpeed[PORT_B] = 0
                 BrickPi.MotorSpeed[PORT_C] = 0
-                BrickPi.MotorSpeed[PORT_D] = 0
-                BrickPiUpdateValues()
+            elif c == 'plot':
+                wsSend("Start plotting...")
+                plot_from_file('coords.csv', self.plotter)
+                print "Done plotting"
+                c = ''
+            elif c == 'zero':
+                wsSend("zero motor positions")
+                set_motor_zero()
+                c = ''
+            elif c == 'plotcircles':
+                wsSend("Plotting circles")
+                plot_circles(self.plotter)
+                c = ''
 
-                print "Motor thread stopped"
-                break  #Exit
+            BrickPiUpdateValues()  # BrickPi updates the values for the motors
+            self.throttle.throttle()  #Don't go too fast.
 
 
 ################## Main #############################
@@ -270,12 +336,12 @@ if __name__ == "__main__":
     motor_B = motorPID_control(PORT_B)
     motor_C = motorPID_control(PORT_C)
     drive_motors = [motor_B, motor_C]
-
     no_values = 1
     while no_values:
         # Make sure we have something before we start running
         # So we wait until no_values goes 0, which means values updated OK
         no_values = BrickPiUpdateValues()
+    set_motor_zero()
 
     # Set up plotting installation
     my_plotter = plot_installation(L_ROPE_0, R_ROPE_0, ROPE_ATTACHMENT_WIDTH)
@@ -290,4 +356,15 @@ if __name__ == "__main__":
 
     application.listen(9093)  # starts the websockets connection
     server_log.newline()  #done setting up. Log it.
-    tornado.ioloop.IOLoop.instance().start()
+    try:
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:  #Triggered by pressing Ctrl+C. Time to clean up.
+        running = False
+        #Shutting down all motors.
+        BrickPi.MotorSpeed[PORT_A] = 0
+        BrickPi.MotorSpeed[PORT_B] = 0
+        BrickPi.MotorSpeed[PORT_C] = 0
+        BrickPi.MotorSpeed[PORT_D] = 0
+        BrickPiUpdateValues()
+
+        print "Motors & motor thread stopped"

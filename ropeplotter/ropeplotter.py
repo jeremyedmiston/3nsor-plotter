d23bac9f29dce6698afe736e839a318abbb81635
Anton Vanhoucke <Anton@fabrique.nl>
2015-09-26 16:25:36 +0200

    circle plotting

--- ropeplotter/ropeplotter.py
+++ ropeplotter/ropeplotter.py
@@ -279,7 +279,6 @@ class RopePlotter(object):
                     # Look at the pixel we're at and move pen up or down accordingly
                     x_norm, y_norm = self.coords_from_motor_pos(self.drive_motors[0].position, self.drive_motors[1].position)
                     pixel_location = (clamp(x_norm * w, (0,w)), clamp(y_norm * w, (0,h)))
-                    print "looking at pixel ", pixel_location
                     if pixels[pixel_location] < 80: # About 33% gray
                         self.pen_motor.run_to_abs_pos(position_sp=DOWN)
                     else:
@@ -321,7 +320,8 @@ class RopePlotter(object):
                 while 1:
                     # Look at the pixel we're at and move pen up or down accordingly
                     x_norm, y_norm = self.coords_from_motor_pos(self.drive_motors[0].position, self.drive_motors[1].position)
-                    pixel_location = (clamp(x_norm * w, (0,w)), clamp(y_norm * w, (0,h)))
+                    pixel_location = (int(clamp(x_norm * w, (0,w))), int(clamp(y_norm * w, (0,h))))
+                    print "looking at pixel ", pixel_location
 
                     if pixels[pixel_location] < 80: # About 33% gray
                         self.pen_motor.run_to_abs_pos(position_sp=DOWN)

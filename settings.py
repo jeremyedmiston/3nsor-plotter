9eab78cecfb67e6719855cd4c6af7b55d33d6655
Anton Vanhoucke <Anton@fabrique.nl>
2017-03-02 10:47:06 +0100

    fabrdam defaults

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 MOTOR_CMD_RATE = 20             # Max number of motor commands per second
-L_ROPE_0 = 44.0                 # Length of left rope in cm when pen is at 0,0 (top left)
-R_ROPE_0 = 62.0                # same for right rope
-ROPE_ATTACHMENT_WIDTH = 53.0    # space between the two attachment points of the plotter.In my case: door width. In cm.
+L_ROPE_0 = 55.0                 # Length of left rope in cm when pen is at 0,0 (top left)
+R_ROPE_0 = 83.0                # same for right rope
+ROPE_ATTACHMENT_WIDTH = 98.0    # space between the two attachment points of the plotter.In my case: door width. In cm.
 CM_TO_DEG = -170
 KP=2
 TI=0

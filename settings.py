e994c01d837116443de89c334f870f0e6309a659
Anton Vanhoucke <Anton@fabrique.nl>
2017-05-01 10:27:27 +0200

    on to chalk

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 MOTOR_CMD_RATE = 20             # Max number of motor commands per second
 L_ROPE_0 = 55.0                 # Length of left rope in cm when pen is at 0,0 (top left)
 R_ROPE_0 = 82.0                # same for right rope
-ROPE_ATTACHMENT_WIDTH = 89.0    # space between the two attachment points of the plotter.In my case: door width. In cm.
+ROPE_ATTACHMENT_WIDTH = 289.0    # space between the two attachment points of the plotter.In my case: door width. In cm.
 CM_TO_DEG = -170
 KP=2
 TI=0
@@ -9,4 +9,4 @@ TD=0
 MAX_SPEED=100
 SCAN_LINES=40
 PREVIEW_SIZE = 160
-CHALK = False
+CHALK = True
